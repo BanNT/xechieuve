035022e718c409e7d54c0bf65b3645ac58e2739c
TranVanHoang <phatradang@gmail.com>
2015-09-17 11:51:54 +0700

    config nhanh khach hang

--- protected/config/main.php
+++ protected/config/main.php
@@ -33,7 +33,7 @@ return array(
         // uncomment the following to enable URLs in path-format
         'urlManager' => array(
             'urlFormat' => 'path',
-            'showScriptName' => FALSE,
+//            'showScriptName' => FALSE,
             'rules' => array(
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
