06684d0cb98565d31c674e2b9797527b3c0e5b32
qkongvan <qkongvan@gmail.com>
2015-10-22 13:52:10 +0700

    sua giao dien chi tiet tin dang

--- protected/views/user/xe_tim_khach/xem_chi_tiet.php
+++ protected/views/user/xe_tim_khach/xem_chi_tiet.php
@@ -8,25 +8,19 @@ $provinces = Province::listProvinces();
             <header>
             <div class="pribox"><?php echo CHtml::encode($tinXTK['tieu_de_tin']); ?></div> </header>
             <section>
-                <table class="table table-striped ct bord">
-                <tr>
-                    <td class="ct1"><span class="blue-text">Tên liên hệ:</span></td>
-                    <td class="ct2"><span class="red-text"><?php echo Chtml::encode($tinXTK['nguoi_lien_lac']); ?></span></td>
-                </tr>
-                <tr>
-                    <td> <span class="blue-text">Điện thoại:</span></td>
-                    <td><span class="red-text"><?php echo Chtml::encode($tinXTK['so_dien_thoai']); ?></span></td>
-                </tr>
-                <tr>
-                    <td><span class="blue-text">Lộ trình:</span></td>
-                    <td><span class="red-text"><?php echo Chtml::encode($tinXTK['dia_chi_di']); ?> - <?php echo CHtml::encode($provinces[$tinXTK['tinh_thanh']]); ?>
-                --> <?php echo Chtml::encode($tinXTK['dia_chi_den']); ?> - <?php echo CHtml::encode($provinces[$tinXTK['noi_den_tinh']]); ?></span></td>
-                </tr>
-                <tr>
-                    <td><h4 class="blue-text">Thông tin thêm: </h4></td>
-                    <td><?php echo $tinXTK['noi_dung_tin']; ?></td>
-                </tr>
-                </table>
+                <p><span class="blue-text">Tên liên hệ: </span>
+                    <span class="red-text"> <?php echo Chtml::encode($tinXTK['nguoi_lien_lac']); ?></span></p>
+                    <br/>
+                <p><span class="blue-text">Điện thoại: </span>
+                    <span class="red-text"><?php echo Chtml::encode($tinXTK['so_dien_thoai']); ?></span></p>
+                    <br/>
+                <p><span class="blue-text">Lộ trình: </span>
+                    <span class="red-text"><?php echo Chtml::encode($tinXTK['dia_chi_di']); ?> - <?php echo CHtml::encode($provinces[$tinXTK['tinh_thanh']]); ?>
+                    <a class="glyphicon glyphicon-arrow-right red-text"></a>
+                    <?php echo Chtml::encode($tinXTK['dia_chi_den']); ?> - <?php echo CHtml::encode($provinces[$tinXTK['noi_den_tinh']]); ?></span></p>
+                    <br/>
+                <p><strong class="blue-text ">Thông tin thêm:</strong><br/>
+                    <span class="red-text"><?php echo $tinXTK['noi_dung_tin']; ?></span></p>
             </section>
         </article>
     </div>
